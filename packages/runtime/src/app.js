86319ca313bad66552eeb7c9b138f321cac51243
danielkasai <daniel.kasai@funkemedien.de>
2026-03-20 17:30:10 +0100

    change app.js - patchDOM

--- packages/runtime/src/app.js
+++ packages/runtime/src/app.js
@@ -1,6 +1,7 @@
 import { destroyDOM } from './destroy-dom.js'
 import { Dispatcher } from './dispatcher.js'
 import { mountDOM } from './mount-dom.js'
+import { patchDOM } from './patch-dom'
 
 export function createApp({ state, view, reducers = {} }) {
     let parentEl = null
@@ -23,18 +24,15 @@ export function createApp({ state, view, reducers = {} }) {
     }
 
     function renderApp() {
-        if (vdom) {
-            destroyDOM(vdom)
-        }
-
-        vdom = view(state, emit)
-        mountDOM(vdom, parentEl)
+        const newVdom = view(state, emit)
+        vdom = patchDOM(vdom, newVdom, parentEl)
     }
 
     return {
         mount(_parentEl) {
             parentEl = _parentEl
-            renderApp()
+            vdom = view(state, emit)
+            mountDOM(vdom, parentEl)
         },
 
         unmount() {
