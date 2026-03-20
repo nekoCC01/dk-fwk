e00893a18faedce2e1ab86f7097060a8c9dcf247
danielkasai <daniel.kasai@funkemedien.de>
2026-03-20 21:41:10 +0100

    recon > arrayDiff function

--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -2,3 +2,13 @@ export function withoutNulls(arr) {
     return arr.filter(item => item != null)
 }
 
+export function arraysDiff(oldArray, newArray) {
+    return {
+        added: newArray.filter(
+            (newItem) => !oldArray.includes(newItem)
+        ),
+        removed: oldArray.filter(
+            (oldItem) => !newArray.includes(oldItem)
+        )
+    }
+}
